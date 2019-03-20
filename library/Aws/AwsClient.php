30732a5fb3bd37d5884a8fbc82486ac70c4ac1d3
Johannes Meyer <johannes.meyer@netways.de>
2019-03-20 11:09:26 +0100

    AwsClient: Allow to connect with Cloudformation and DynamoDB

--- library/Aws/AwsClient.php
+++ library/Aws/AwsClient.php
@@ -245,6 +245,18 @@ class AwsClient
         return $this->sortByName($objects);
     }
 
+    public function getStacks()
+    {
+        $client = $this->client()->get('Cloudformation');
+        $res = $client->describeStacks();
+        return $res['Stacks'];
+    }
+
+    public function getDynamoDb()
+    {
+        return $this->client()->get('DynamoDb');
+    }
+
     public static function enumRegions()
     {
         return array(
