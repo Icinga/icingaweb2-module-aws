d74bd53a30b4a76f3a32053df0e5c274af1ec3a2
Marius Hein <mhein@itsocks.de>
2019-08-12 10:28:54 +0200

    Add composer files

--- run.php
+++ run.php
@@ -1,3 +1,6 @@
 <?php
+namespace Icinga\Module\Aws {
+    require_once __DIR__ . '/library/vendor/autoload.php';
 
-$this->provideHook('director/ImportSource');
+    $this->provideHook('director/ImportSource');
+}
