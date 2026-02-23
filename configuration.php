b170eb776a12c1fef0b15feb64f4a7d5f782b639
Eric Lippmann <eric.lippmann@icinga.com>
2026-02-23 21:46:35 +0100

    License source files as GPL-3.0-or-later
    Add SPDX license headers and mark source files as GPL-3.0-or-later to
    preserve the option to relicense under later GPL versions.

--- configuration.php
+++ configuration.php
@@ -1,5 +1,8 @@
 <?php
 
+// SPDX-FileCopyrightText: 2019 Icinga GmbH <https://icinga.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 $this->provideConfigTab('keys', array(
     'title' => $this->translate('Configure your AWS access keys'),
     'label' => $this->translate('AWS Keys'),
