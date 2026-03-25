786a07b082ba686f9978e429873f6f185b217640
Eric Lippmann <eric.lippmann@icinga.com>
2026-03-25 10:14:12 +0100

    Drop support for manual installations of the AWS SDK; Composer only (#62)
    Manual installations do not support version constraints and require additional
    code. I see no reason to continue supporting this.
    
    Additionally, the expected installation path has changed — any existing AWS SDK
    installation, whether manual or via Composer, will be ignored, requiring a
    fresh installation.

--- run.php
+++ run.php
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2018 Icinga GmbH <https://icinga.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-namespace Icinga\Module\Aws {
-    $this->provideHook('director/ImportSource');
-}
+require_once __DIR__ . '/vendor/autoload.php';
+
+/** @var $this \Icinga\Application\Modules\Module */
+$this->provideHook('director/ImportSource');
